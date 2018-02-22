cfe53b3988532513b4583b1b1c1f25d3dcab47ee
OstenTV <info@ostentv.dk>
2018-02-22 12:45:55 +0100

    Added a TCP listener.

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace Server
 {
@@ -10,6 +12,24 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting ChatApp server. . .");
+
+            // Define which interface to use and what port to listen on.
+            System.Net.IPAddress serverAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            int port = 4444;
+
+            // Create the listener.
+            System.Net.Sockets.TcpListener listener = new System.Net.Sockets.TcpListener(serverAddress, port);
+            Console.WriteLine("Listener created.");
+
+            // Start the listener.
+            Console.WriteLine("Starting listener. . .");
+            listener.Start();
+            Console.WriteLine("Listening on: " + serverAddress + ":" + port);
+
+            TcpClient client = listener.AcceptTcpClient();
+
+
         }
     }
 }
